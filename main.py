1ace4c6e767a1f3efa08a20b6c8bfb6b4ac25724
Gennadiy Shpak <gena_shpak@ukr.net>
2024-05-09 20:52:45 +0300

    Comm_09.05.24_20.52

--- main.py
+++ main.py
@@ -70,6 +70,6 @@ async def healthchecker(db: AsyncSession = Depends(get_db)):
 
 
 # if __name__ == "__main__":
-#     # uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+#     uvicorn.run("main:app", host="localhost", port=8000, reload=True)
 #     # uvicorn.run("main:app", host="0,0,0,0", port=int(os.environ.get("PORT", 8000)))
 #     uvicorn.run("main:app", host="0,0,0,0", port=8000, reload=True, log_level="info")
